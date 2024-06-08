f02e867b2f1ca9439af6767900af33808dff1942
kartikkumar6060 <kartik2005320@gmail.com>
2024-06-08 11:57:22 +0530

    js

---
+++ index.js
@@ -0,0 +1,149 @@
+// // var num=10;
+// // {
+
+// // num=20;
+// // console.log(num);
+// // }
+
+// // console.log("var",num)
+
+// // let num1=10;
+// // {
+// //     let num1=20;
+// //     num1=30;
+// //     console.log(num1);
+// // }
+
+// // console.log("let",num1)
+
+
+// // const num2={"name":"RA"}
+// // {
+// //     num2.name="RAMESH"
+// // }
+
+// // console.log("const",num2)
+
+// console.log("SUM:")
+// {
+// let n1=10;
+
+// let n2=20;
+//  let sum=n1+n2;
+// console.log(sum)
+// }
+
+
+
+// console.log("SUBTRACT:")
+// {
+// let n1=10;
+
+// let n2=20;
+// let sub=n1-n2;
+// console.log(sub)}
+
+// console.log("multiply:")
+
+// {
+//     let n1=10;
+
+// let n2=20;
+//  let mul=n1*n2;
+// console.log(mul)
+// }
+
+
+// console.log("divide:")
+// {let n1=100;
+
+// let n2=20;
+//  let div=n1/n2;
+// console.log(div)}
+
+// console.log("mod:")
+// {let n1=10;
+
+// let n2=4;
+//  let mod=n1%n2;
+// console.log(mod)}
+
+
+// let checkin="hello";
+// let checkout=10;
+// console.log(checkin)
+// console.log(checkout)
+// console.log(!checkin)
+
+// let a=true;
+
+// let b=false;
+
+// console.log(a&&b)
+
+// let a=10;
+
+// let b=20;
+
+// let result = (a > b) ? true : false;
+
+// console.log(result);
+
+
+// let a=10;
+
+// let b=20;
+
+// let c=30;
+
+// max = (a > b) ? 
+// (a > b ? a : c) : 
+// (b > c ? b : c);
+
+// console.log(max);
+
+// let a=10;
+
+// let b=20;
+
+// let c=30;
+
+
+// let max=(a>b&&a>c)?a:(b>c)?b:c;
+
+// console.log(max);
+
+// let a=10;
+
+// let b=20;
+
+// let c=30;
+
+
+// let max=(a<b&&a<c)?a:(b<c)?b:c;
+
+// console.log(max);
+
+
+// let age=10;
+
+// let vote=(age>=18)?"you can vote":"you can't vote";
+// console.log(what is your age ?);
+// console.log(vote);
+
+
+
+
+// let a=10;
+// let b=10;
+
+//  let result=(a&&b)?"yes":"no";
+//  console.log(result);
+
+
+let a=10;
+a++;
+console.log(++a);
+
+a--;
+console.log(--a);
